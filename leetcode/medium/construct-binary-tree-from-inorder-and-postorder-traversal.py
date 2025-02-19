1d4afa12a9e3494cd5fc4f9968889c5b2f685d15
ARTEM <sorintema@gmail.com>
2025-02-19 22:39:59 +0300

    added task

--- leetcode/medium/construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ leetcode/medium/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -3,7 +3,8 @@ from typing import List, Optional
 from helpers.TreeNode import TreeNode
 
 
-class Solution:
+# time: O(n2)
+class Solution2:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         if not inorder:
             return None
@@ -14,3 +15,26 @@ class Solution:
         root.left = self.buildTree(inorder[:index], postorder)
 
         return root
+
+
+# time: O(n)
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        mapping = {}
+        for i in range(len(inorder)):
+            mapping[inorder[i]] = i
+
+        def build(left, right):
+            if left > right:
+                return None
+
+            root = TreeNode(postorder.pop())
+            index = mapping[root.val]
+            root.right = build(index + 1, right)
+            root.left = build(left, index - 1)
+            return root
+
+        return build(0, len(postorder) - 1)
+
+s = Solution()
+print(s.buildTree([9,3,15,20,7], [9,15,7,20,3]))
