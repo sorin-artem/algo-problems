b51e9f6be87d61127b94759c6b77b28cdee623b6
ARTEM <sorintema@gmail.com>
2025-03-29 00:54:52 +0300

    added tasks

---
+++ leetcode/hard/maximum-number-of-points-from-grid-queries.py
@@ -0,0 +1,26 @@
+from heapq import heappop, heappush
+from typing import List
+
+
+class Solution:
+    def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
+        rows = len(grid)
+        cols = len(grid[0])
+        q = [(n, i) for i, n in enumerate(queries)]
+        q.sort()
+        min_heap = [(grid[0][0], 0, 0)]
+        res = [0] * len(queries)
+        points = 0
+        visit = set([(0, 0)])
+        for limit, index in q:
+            while min_heap and min_heap[0][0] < limit:
+                var, r, c = heappop(min_heap)
+                points += 1
+                neighbors = [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]]
+                for nr, nc in neighbors:
+                    if (0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in visit):
+                        heappush(min_heap,(grid[nr][nc], nr, nc))
+                        visit.add((nr, nc))
+            res[index] = points
+
+        return res
---
+++ leetcode/medium/minimum-index-of-a-valid-split.py
@@ -0,0 +1,17 @@
+from collections import defaultdict, Counter
+from typing import List
+
+
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        left = defaultdict(int)
+        right = Counter(nums)
+        for i in range(len(nums)):
+            left[nums[i]] += 1
+            right[nums[i]] -= 1
+            left_len = i + 1
+            right_len = len(nums) - i - 1
+            if 2 * left[nums[i]] > left_len and 2 * right[nums[i]] > right_len:
+                return i
+
+        return -1
